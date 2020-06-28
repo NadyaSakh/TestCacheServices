c4a3c2f8eb2ce6ad4093a3eb9e34d235e609f3bc
nadya.sakharova <nadezhda.sakharova@fogstream.ru>
2020-06-28 19:28:52 +1000

    Добавьте файлы проекта.

---
+++ TestCacheServices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCacheServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
